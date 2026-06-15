d279ebf263bd5eb9c4a0ccc576adcaa412f41e70
judemjt <judetanguy@gmail.com>
2026-06-15 05:08:37 +0100

    Update auth.js

--- public/js/auth.js
+++ public/js/auth.js
@@ -63,7 +63,34 @@ function protectPage() {
         window.location.href = "login.html";
     }
 }
+async function login() {
+    try {
+        const email = document.getElementById("email").value;
+        const motDePasse = document.getElementById("password").value;
 
+        const res = await fetch(`${API}/auth/login`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ email, motDePasse })
+        });
+
+        const data = await res.json();
+
+        if (!res.ok) {
+            alert(data.message || "Erreur login");
+            return;
+        }
+
+        localStorage.setItem("token", data.token);
+        localStorage.setItem("user", JSON.stringify(data.user));
+
+        window.location.href = "dashboard.html";
+
+    } catch (err) {
+        console.error(err);
+        alert("Erreur serveur");
+    }
+}
 
 // =========================
 // 🚀 AUTO INIT
